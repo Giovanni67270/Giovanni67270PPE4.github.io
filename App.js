6b3aa7679a4bcdc776207c6474ff1137533884e7
Giovanni67270 <82649136+Giovanni67270@users.noreply.github.com>
2021-05-07 09:19:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    
+    <WebView
+    source={{ uri: 'https://yass-stonks.com/PPE/' }}
+    style={{ marginTop: 20 }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
